2f6fb2e510aef9ef4809a3e9f2e4987f60091f1f
ekatsevi <katsevich.gene@gmail.com>
2022-06-08 14:37:25 -0400

    Add module load command to run_all.sh

--- run_all.sh
+++ run_all.sh
@@ -1,5 +1,10 @@
 # runs all downloading and processing scripts
 
+hpcc=$(hostname | grep "hpcc" | wc -l)
+if [[ hpcc ]]
+then
+  module load R/R-4.1.2
+fi
 Rscript create_dirs_1.R
 Rscript download_raw_2.R
 Rscript create_odms_3.R
